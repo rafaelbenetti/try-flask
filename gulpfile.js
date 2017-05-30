19604b05bb4a90ac35f979ece796eb3147067877
Rafael de Oliveira Benetti <rafaeldeoliveirabenetti@gmail.com>
2017-05-29 23:25:41 -0300

    chore(config) add front structure base

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+(function () {
+    'use strict';
+
+    const gulp = require('gulp');
+    const minifyCss = require('gulp-clean-css');
+    const uglify = require('gulp-uglify');
+    const del = require('del');
+    const rename = require('gulp-rename');
+    const jshint = require('gulp-jshint');
+    const plumber = require('gulp-plumber');
+    const stylish = require('jshint-stylish')
+    const concat = require('gulp-concat');
+    const sass = require('gulp-sass');
+    const babel = require('gulp-babel');
+    const chmod = require('gulp-chmod');
+    const postcss = require('gulp-postcss');
+    const autoprefixer = require('autoprefixer');
+
+    const publicFolder = 'public/';
+    const scriptsPath = [publicFolder + 'js/*.js'];
+
+    gulp.task('delete', () => {
+        del([publicFolder + 'assets/css',
+            publicFolder + 'assets/js'
+        ], (err) => {
+            console.log('Files deleted');
+        });
+    });
+
+    gulp.task('style', () => {
+        return gulp
+            .src(publicFolder + 'scss/*.scss')
+            .pipe(concat('style.css'))
+            .pipe(sass().on('error', sass.logError))
+            .pipe(postcss([autoprefixer]))
+            .pipe(minifyCss())
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            .pipe(chmod(666))
+            .pipe(gulp.dest(publicFolder + 'assets/css'));
+    });
+
+    gulp.task('script', () => {
+        return gulp
+            .src(scriptsPath)
+            .pipe(plumber())
+            .pipe(jshint({
+                esversion: 6
+            }))
+            .pipe(jshint.reporter(stylish))
+            .pipe(babel({
+                presets: ['es2015']
+            }))
+            .pipe(uglify())
+            .pipe(concat('script.js'))
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            .pipe(chmod(666))
+            .pipe(gulp.dest(publicFolder + 'assets/js'));
+    });
+
+    gulp.task('watch', () => {
+        gulp.watch(publicFolder + 'scss/*.scss', ['style']);
+        gulp.watch(scriptsPath, ['script']);
+    });
+
+    gulp.task('default', ['delete', 'style', 'script', 'watch']);
+})();
---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    function init() {
+        console.log('init with sucess');
+    }
+
+    init();
+})();
